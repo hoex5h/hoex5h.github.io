109dc15c42d4ca6ed111c2301d55543062c13f08
hoeeeeeh <hoeeeeeh@naver.com>
2025-01-15 13:33:13 +0900

    chore: upload cover image

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -184,7 +184,7 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
             }
         );
 
-        cover = `assets/images/${cover}.png`;
+        cover = `assets/images/cover/${cover.toLowerCase()}.png`;
 
         const fm = `---
 layout: post
