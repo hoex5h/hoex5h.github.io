61efd6e1f67ec08a2efefda837bbfb1f40e2a28b
hoex5h <hoex5h@users.noreply.github.com>
2025-01-15 07:23:03 +0000

    deploy: a92a5d121ed486872da818e158fd1c2ff73fdced

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -2,21 +2,14 @@
 var documents = [
     
 
+
+
+
+
+
+
 {
     "id": 0,
-    "url": "https://hoex5h.github.io//404.html",
-    "title": "404 - Page Not Found",
-    "body": "&lt;!DOCTYPE html&gt;       404 - Page Not Found                                                                                                                                                               404          Page not found           Go to the front page →                                                                                                                                                                                              환경                                                                과학                                                      두 번째 지구는 없다 - 타일러 라쉬:                                               두 번째 지구는 없다 - 타일러 라쉬                                                                                                                      3 min read                                                                  "
-    
-},
-
-
-
-
-
-
-{
-    "id": 1,
     "url": "https://hoex5h.github.io//",
     "title": "",
     "body": "                                                  HOEH 개발 블로그:                           Home  About                                                                                                                                              환경                                                        과학                                              두 번째 지구는 없다 - 타일러 라쉬:                                       두 번째 지구는 없다 - 타일러 라쉬                                                                                                                                     김영길                                                                                   3 min read                                    "
@@ -26,7 +19,7 @@ var documents = [
 
 
 {
-    "id": 2,
+    "id": 1,
     "url": "https://hoex5h.github.io//about/",
     "title": "About",
     "body": "Ghost is professional publishing platform designed for modern journalism. This is a demo site of a basic Ghost install to give you a general sense of what a new Ghost site looks like when set up for the first time.  If you’d like to set up a site like this for yourself, head over to Ghost. org and start a free 14 day trial to give Ghost a try! If you’re a developer: Ghost is a completely open source (MIT) Node. js application built on a JSON API with an Ember. js admin client. It works with MySQL and SQLite, and is publicly available on Github. If you need help with using Ghost, you’ll find a ton of useful articles on our knowledgebase, as well as extensive developer documentation. "
@@ -42,7 +35,7 @@ var documents = [
 
 
 {
-    "id": 3,
+    "id": 2,
     "url": "https://hoex5h.github.io//author/hoeeeeeh/",
     "title": "Author Page",
     "body": "{% for author in site. data. authors %}  {% if author[1]. username == page. author %}  &lt;header class= site-header outer {% if author[1]. cover or page. cover %}  style= background-image: url({{ site. baseurl }}{% if author[1]. cover %}{{ author[1]. cover }}{% elsif page. cover %}{{ page. cover }}{% endif %}) {% else %}no-cover{% endif %} &gt;    &lt;div class= inner &gt;      {% include site-nav. html %}      &lt;div class= site-header-content &gt;        {% if author[1]. picture %}                  {% endif %}        &lt;h1 class= site-title &gt;{{ author[1]. name }}&lt;/h1&gt;        {% if author[1]. bio %}          &lt;h2 class= author-bio &gt;{{ author[1]. bio }}&lt;/h2&gt;        {% endif %}        &lt;div class= author-meta &gt;          {% if author[1]. location %}            &lt;div class= author-location &gt;{{ author[1]. location }} •&lt;/div&gt;          {% endif %}          &lt;div class= author-stats &gt;            {% if paginator. total_posts == 0 %}No posts{% elsif paginator. total_posts == 1 %}1 post{% else %}{{ paginator. total_posts }} posts{% endif %} •          &lt;/div&gt;          {% if author[1]. url %}            {% include website. html %}          {% endif %}          {% if author[1]. twitter %}            {% include twitter. html %}          {% endif %}          {% if author[1]. facebook %}            {% include facebook. html %}          {% endif %}          {% include rss. html %}        &lt;/div&gt;      &lt;/div&gt;    &lt;/div&gt;  &lt;/header&gt;  {% endif %}{% endfor %}                   {% include post-card. html %}      "
@@ -70,7 +63,7 @@ var documents = [
 
 
 {
-    "id": 4,
+    "id": 3,
     "url": "https://hoex5h.github.io//%EB%91%90_%EB%B2%88%EC%A7%B8_%EC%A7%80%EA%B5%AC%EB%8A%94_%EC%97%86%EB%8B%A4_-_%ED%83%80%EC%9D%BC%EB%9F%AC_%EB%9D%BC%EC%89%AC",
     "title": "두 번째 지구는 없다 - 타일러 라쉬",
     "body": "2025/01/14 - 두 번째 지구는 없다 - 타일러 라쉬두 번째 지구는 없다. 말 그대로 지금 사는 지구가 사라지면, 대체재는 없다는 소리다. 환경을 아끼고 심각성을 인지해야하며, 그저 개개인이 자원을 아껴쓰는 것만으로는 부족하다는 이야기가 주를 이룬다.  코로나의 근본적인 원인은 기후위기 이다. 사실 이 문장에서 핵심인 단어는 ‘근본적인’ 이라고 생각한다. 코로나 발병에 있어서 가장 높은 영향력을 끼친 원인이 기후위기라고 해석할 수 있는데, 기후 위기로 인해 박쥐의 서식지가 점점 인간쪽에 가까워졌기 때문에 기후위기가 원인이라고 말하는 듯 하다. 이 말에 동의하긴 하지만 ‘근본적인 원인’ 이라고 할 수 있을까? 적합한 조리 과정을 통한 박쥐였다면 (나는 안먹을테지만) 문제가 없었을 것이다. 그저 비위생적인 식용 습관에 의해 발병된 질병이지 않나 생각한다. 박쥐는 동굴같은 비위생적인 환경에 서식하기 때문에 온갖 질병의 매개체가 되기 쉬우면서도 체온은 다른 포유류보다 2~3도 높기 때문에 면역체계가 매우 뛰어나 본인이 질병을 앓지는 않는다고 한다.  78억명의 빚쟁이한 해 동안 지구가 생산할 수 있는 자원의 양이 1 이라고 하면, 우리를 포함한 전 세계인들은 매년 1. 75를 사용하고 있다고 한다. (코로나 때문에 가동을 멈춘 공장들이 늘어나면서 이례적으로 2020년부터는 조금 줄어들었다고 한다. ) 자원이 순환되며 지구가 정상적으로 재생하기 위해서 1보다 같거나 적게 사용해야 하지만 우리는 미래의 0. 75를 계속 빌려다 쓰고 있는 것이다.  경제의 관념5,500원 짜리 바닐라 라떼  가격에 반영된 것 : 바닐라 라떼에 들어가는 재료비, 기타 노동비 …   가격에 반영되지 않았지만 얻은 것 : 카페인 섭취로 인한 업무능력 향상, 휴식, …      경제적 외부성이라는 개념이 있다. 제삼자가 외부의 영향으로 비용이나 이익이 생기지만, 그것을 통제도 제어도 할 수 없어서 공식에 반영되지 않고, 실제 가격이나 값을 계산하는 데에 포함되지 않는다.     가격에 반영되지 않았지만 잃은 것 : 바닐라 라떼가 건강에 미치는 영향, 먼 훗날 늘어날 의료비용, 아메리카노를 시켰다면 아낄 수 있었던 1,000원의 경제적 여력…우리는 계산이 어려운 이런 것들에 전부 ‘경제적 외부성’ 이라는 라벨을 붙여서 무시하는 경제관이 있다. 환경  커피 원두는 열대 지역에서 자라기 떄문에 열대 지역에 커피나무를 심고 키워야 한다. 원래 그 자리에서 자라던 식물이 생태계에서 했던 역할의 값 다른 식물을 제거하고 커피나무를 키웠을 때 해당 지역 공기의 질 커피를 다른 나라에 수출하는 과정에서 생기는 온실가스 대기로 방출된 가스 때문에 대기가 따뜻해지고 강우 패턴이 달라져서 가뭄을 유발하고 해충이 증가 하는 값… 등등 타일러는 경제 활동의 외부 효과를 전혀 고려하지 않는 것, 어떤 일이 유발하는 환경오염과 그것을 회복하는 데 드는 시간과 비용을 염두에 두지 않는 것이 환경 문제의 핵심이라고 한다.    개인이 아껴쓰는 걸로는 부족하다   개인이 종이컵같은 일회용품 좀 덜 쓰고, 물 아껴쓰고, 환경에 관심 좀 갖는 걸로는 부족하다고 얘기한다. 그는 환경을 아끼는 기업을 우대하고, 반대의 기업에는 제재를 내리는 등의 관리가 필요하며 그것이 이루어지기 위해서는 우리가 환경에 관심을 갖는 것을 넘어 기업에게, 정부에게 요구해야한다고 한다. (나는 안했지만) 일본 불매운동 처럼 누군가가 무언가에 투쟁하여 얻어내고자 하는 것처럼 말이다.   나는 이 때 일본 여행을 다녀왔으며 후쿠오카 유후인의 료칸에서 정말 즐거운 시간을 보냈다. 정말 최고다. 또 가고 싶다. 거기서 닌텐도도 샀는데, 닌텐도 라이트여서 TV에 연결이 안되는 바람에 중고로 처분했었던 기억이 있다.     환경을 아껴야한다고 누구나 다 말하지는 않는다.   기온이 바뀌게 되면 불리해지는 곳이 있는 반면, 유리해지는 곳도 있다. 예를 들어 러시아 같은 경우 기후위기가 진행될 수록 북극에 해상로가 열리고 부동항이 생긴다고 한다. 최근 우크라이나를 침공한 이유를 생각해보면 더더욱 이해가 쉬울 듯 하다.     환경을 위한 채식도 존중받아야 한다.   축산업은 온실가스 배출에 큰 비중을 차지한다. 자동차의 매연이나 석유가 기후 위기의 주범이 맞긴 하지만 축산업 또한 온실가스 배출량의 18%를 차지할 정도로 비중이 크다. 혹자는 51%에 라고 주장하기도 했다고 한다. 고로 자신의 건강을 위한 채식이 아니라 환경을 위한 채식도 존중받아야 한다고 한다.   나는 남에게 피해를 끼치지 않는 개인의 신념은 존중받아 마땅하다고 생각한다. 다만 한국만큼 모임자리에서 육류를 많이 섭취하는 나라가 또 있을까 싶기 때문에 ‘사회적인 육식’ 이 있을 수 밖에 없다고 생각한다. 타일러도 마찬가지의 이유로 채식을 포기했다고 한다.     FSC 인증 종이   FSC 인증 마크가 붙은 종이는, 이 종이를 사용하기 위해 벌목한 만큼 다시 심었다는 것이 인증된 마크라고 한다. 학창 시절부터 지금까지 수도 없이 많은 종이를 써왔을 나도 처음듣는 종이였다. 이 책은 FSC 인증 종이로 만들어졌다고 한다.   FSC 인증 종이는 당연히 일반 종이보다 비쌀텐데 FSC 인증 여부만 다른 똑같은 책이 있다면 과연 나는 어떤 선택을 할 것인가?  "
